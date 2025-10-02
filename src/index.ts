2e3bcb928fd1d9840164bf9f004e0575d676c516
Kari3600 <karol.xvx@gmail.com>
2025-10-02 21:54:25 +0200

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import * as core from '@actions/core';
+import * as fs from 'fs';
+
+async function run() {
+  try {
+    const repos = core.getInput('repositories').split(',');
+
+    // Generate settings.xml
+    let xml = `<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd">
+      <servers>\n`;
+
+    for (const repo of repos) {
+      xml += `<server>
+  <id>${repo}</id>
+  <username>\${env.GITHUB_ACTOR}</username>
+  <password>\${env.GITHUB_TOKEN}</password>
+</server>\n`;
+    }
+
+    xml += `</servers></settings>`;
+
+    fs.mkdirSync(`${process.env.HOME}/.m2`, { recursive: true });
+    fs.writeFileSync(`${process.env.HOME}/.m2/settings.xml`, xml);
+
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
